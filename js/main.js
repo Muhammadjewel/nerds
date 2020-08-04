97ef681b735481c52b69cebc271e108ebe96dccb
muhammadjewel <msuratov@gmail.com>
2020-08-04 22:24:41 +0500

    Open sitenav__list on sitenav__menu-toggler click. Change sitenav background-color in small screens when sitenav__list is open.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// Sitenav
+var elSitenav = document.querySelector('.sitenav');
+var elSitenavMenuToggler = elSitenav.querySelector('.sitenav__menu-toggler');
+var elSitenavList = document.querySelector('.sitenav__list');
+
+elSitenavMenuToggler.addEventListener('click', function () {
+  elSitenav.classList.toggle('sitenav--list-open');
+  elSitenavList.classList.toggle('sitenav__list--open');
+});
+
+
+// Promo slider
 var promoSlider = tns({
   container: '.promo-slider__track',
   controls: false,
